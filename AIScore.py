2dbb2fa2f58c2e986e18e221466a24e2a77a22ee
thompty <thompty@iu.edu>
2025-09-11 12:13:56 -0400

    Scoring Reqs

---
+++ AIScore.py
@@ -0,0 +1,10 @@
+#Scoring Metrics (With Latency):
+#Ramp Up Time
+#Bus Factor
+#License Compatibility
+#Size Score
+#Dataset & Code Availability
+#Dataset Quality
+#Code Quality
+#Performance Claims
+
